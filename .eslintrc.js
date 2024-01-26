220c77d781a08fd250416996c515909b3377ca6e
Misa Liu <misaliu@misaliu.top>
2024-01-26 20:50:05 +0800

    chore: Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+  extends: [ 'eslint-config-airbnb-base', 'plugin:@typescript-eslint/recommended' ],
+  parser: '@typescript-eslint/parser',
+  plugins: [ '@typescript-eslint' ],
+  rules: {
+    'import/extensions': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { ignoreTypeReferences: true }],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [ '.js', '.ts' ],
+        moduleDirectory: [ 'node_modules', './src' ],
+      },
+    },
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+}
